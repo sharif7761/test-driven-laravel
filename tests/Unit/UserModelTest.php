dec4a3969816f534cb02b0a002fff186c296d527
Puspita Vabna <sharif7761@gmail.com>
2021-11-08 23:17:23 +0600

    user full name test

--- tests/Unit/UserModelTest.php
+++ tests/Unit/UserModelTest.php
@@ -2,17 +2,27 @@
 
 namespace Tests\Unit;
 
+use \App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use PHPUnit\Framework\TestCase;
 
 class UserModelTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function user_has_full_name_attribute()
     {
-        $this->assertTrue(true);
+        $user = User::create([
+            'name' => 'Sharif',
+            'lastname' => 'Ahmed',
+            'email' => 'sharif@gmail.com',
+            'password' => '12345678'
+        ]);
+
+        $this->assertEquals('Sharif Ahmed', $user->fullname);
     }
 }
